c2b70703e17700ae4275b5b9c1cba86de674e601
Jean Dresbach <jean.rafael.dresbach@gmail.com>
2026-05-17 20:00:34 -0300

    aprendendo controle de fluxo basico

--- curso-angular/src/app/app.ts
+++ curso-angular/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 // import { FirstComponent } from "./first-component/first-component";
 // import { DataBindings } from './data-bindings/data-bindings';
-import { Formulario } from './formulario/formulario';
+// import { Formulario } from './formulario/formulario';
+import { ControleFluxo } from './controle-fluxo/controle-fluxo';
 
 @Component({
   selector: 'app-root',
-  imports: [Formulario],
+  imports: [ControleFluxo],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
